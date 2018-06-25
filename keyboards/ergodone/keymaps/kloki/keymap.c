b7e38d0142535aa0bcdad450c97e586ed8ec4c27
Koen Klinkers <k.klinkers@gmail.com>
2018-06-25 16:38:34 +0200

    added menukey

--- keyboards/ergodone/keymaps/kloki/keymap.c
+++ keyboards/ergodone/keymaps/kloki/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,        KC_D,     KC_R,   KC_W,   KC_B,   KC_GRV,
         CTL_T(KC_ESC),  KC_A,        KC_S,     KC_H,   KC_T,   KC_G,
         KC_LSFT,        KC_Z,        KC_X,     KC_M,   KC_C,   KC_V,   KC_EQL,
-        KC_MEH,         KC_LGUI,     KC_LALT,  KC_LEFT,KC_RGHT,
+        KC_MENU,        KC_LGUI,     KC_LALT,  KC_LEFT,KC_RGHT,
                                                        KC_ESC,  KC_HOME,
                                                                 KC_END,
                             LT(SYMB, KC_SPC),   LGUI_T(KC_ENT), S(KC_LGUI),
